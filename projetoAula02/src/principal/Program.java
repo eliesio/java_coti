fd60228b6dfe0878699e10ca99b48827959a0f19
Eliesio Lima <Eliesio@Jaisons-MacBook-Pro.local>
2021-07-29 21:46:47 -0300

    Segundaa parte da aula02

--- projetoAula02/src/principal/Program.java
+++ projetoAula02/src/principal/Program.java
@@ -1,22 +1,35 @@
 package principal;
 
-import entities.Pessoa;
+import java.util.Date;
+
+import entities.Funcao;
+import entities.Funcionario;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		// criando um objeto da classe Pessoa
-				Pessoa pessoa = new Pessoa(1, "Sergio Mendes", "123.456.789-00");
 
-				//imprimindo os dados de pessoa..
-				System.out.println("\nDADOS DE PESSOA:\n");
-				
-				System.out.println("ID....: " + pessoa.getId());
-				System.out.println("NOME..: " + pessoa.getNome());
-				System.out.println("CPF...: " + pessoa.getCpf());
+		// criando um objeto da classe Funcionario
+		Funcionario funcionario = new Funcionario(1, "Sergio Mendes", "123.456.789-00", "12345", 2000.0, new Date());
+		//criando um objeto da classe Funcao
+		Funcao funcao = new Funcao(1, "PROG", "Programador JAVA");
 		
-				
+		//associando o funcionario com a função
+		funcionario.setFuncao(funcao);
+		
+		//imprimindo os dados de pessoa..
+		System.out.println("\nDADOS DE FUNCIONARIO:\n");
+		
+		System.out.println("ID..............: " + funcionario.getId());
+		System.out.println("NOME............: " + funcionario.getNome());
+		System.out.println("CPF.............: " + funcionario.getCpf());
+		System.out.println("MATRICULA.......: " + funcionario.getMatricula());
+		System.out.println("SALARIO.........: " + funcionario.getSalario());
+		System.out.println("ADMISSÃO........: " + funcionario.getDataAdmissao());
+		
+		System.out.println("ID DA FUNÇÃO....: " + funcionario.getFuncao().getId());
+		System.out.println("SIGLA...........: " + funcionario.getFuncao().getSigla());
+		System.out.println("DESCRIÇÃO.......: " + funcionario.getFuncao().getDescricao());
+		
 	}
-
 }
