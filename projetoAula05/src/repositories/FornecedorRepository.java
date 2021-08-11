1df17ce6425774812079f2d8a4df0ac8a3710aa3
eliesio <eliesio.jtl@gmail.com>
2021-08-10 22:05:20 -0300

    Segunda parte da estrutura do crud

---
+++ projetoAula05/src/repositories/FornecedorRepository.java
@@ -0,0 +1,34 @@
+package repositories;
+
+import java.util.List;
+
+import entities.Fornecedor;
+import interfaces.IFornecedorRepository;
+
+public class FornecedorRepository implements IFornecedorRepository {
+
+	@Override
+	public void create(Fornecedor obj) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Fornecedor obj) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Fornecedor obj) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Fornecedor> read() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ projetoAula05/src/repositories/ProdutoRepository.java
@@ -0,0 +1,34 @@
+package repositories;
+
+import java.util.List;
+
+import entities.Produto;
+import interfaces.IProdutoRepository;
+
+public class ProdutoRepository implements IProdutoRepository {
+
+	@Override
+	public void create(Produto obj) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Produto obj) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Produto obj) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Produto> read() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
